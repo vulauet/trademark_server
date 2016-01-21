9a5b75c12ae0ddd1f4530608c300a46cd3dbd06a
Le Anh Vu <vula@Macbook-Air.local>
2016-01-21 15:31:26 +0700

    Image uploadable

--- testClient.py
+++ testClient.py
@@ -2,7 +2,16 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('112.137.130.32', 12055))
-s.send('test')
+# s.send('test')
+data = 'BT.jpg'
+imp = open(data, 'r')
+while True:
+	strng = imp.readline(512)
+	if not strng:
+		break
+	s.send(strng)
+imp.close()
 result = s.recv(1024)
 print result
 s.close()
+exit()
--- testServer.py
+++ testServer.py
@@ -1,4 +1,5 @@
 import SocketServer
+import os
 
 class MyTCPServer(SocketServer.ThreadingTCPServer):
 	allow_reuse_address = True
@@ -6,8 +7,18 @@ class MyTCPServer(SocketServer.ThreadingTCPServer):
 class MyTCPServerHandler(SocketServer.BaseRequestHandler):
 	def handle(self):
 		try:
-			inp = self.request.recv(1024).strip()
-			print inp
+			# inp = self.request.recv(1024).strip()
+			
+			fname = "logo.jpg"
+			fp = open(fname, 'w')
+			while True:
+				strng = self.request.recv(512)
+				if not strng:
+					break
+				fp.write(strng)
+			fp.close()
+			# im = Image.open(fname)
+			# im.save("logo", "JPEG")
 			self.request.sendall("Received")
 		except Exception, e:
 			print "[e] Excetion while receiving message: ", e
